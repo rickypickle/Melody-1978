6277e8a995174d2277b2e287a4249467ccef96d6
Liam McCreith <rickyroma@gmail.com>
2013-07-01 00:57:16 +0100

    Added Opening Cutscene

---
+++ Assets/Test/Scenes/CutScenes/Trailer Stuff/Belt/Box.js
@@ -0,0 +1,10 @@
+#pragma strict
+
+var speed = 12.0;
+//var Model : GameObject;
+
+function Update () 
+{
+	transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * speed);
+	transform.Translate(Input.GetAxis("Horizontal") * Vector3.right * speed);
+}
